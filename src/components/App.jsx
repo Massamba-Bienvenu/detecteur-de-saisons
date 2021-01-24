3923d9d4c52d877751cbb05797f82dfbf02b46ec
Bienvenu Massamba <boukamass@gmail.com>
2021-01-24 00:54:06 -0600

    Specifying default props for the spinner component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,7 +25,7 @@ class App extends React.Component {
       return <SeansonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
